0261191fb0cbbde6cad23f615470a0a2122850c3
Lskar <3074524288@qq.com>
2025-11-12 12:20:21 +0800

    引入分布式锁，提升系统的并发度

---
+++ BackEnd/src/main/java/com/teamup/practice/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.teamup.practice.constant;
+
+/**
+ * Redis缓存常量
+ */
+public class RedisCacheConstant {
+
+
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register";
+}
